ec685820359cfe2ac169f4160f7180f25810a26e
patrickvrb <patrickvrb@gmail.com>
2021-07-03 13:04:04 -0300

    Set e remove favorite em exibição por lista

--- src/app/components/chatbot-as-list/chatbot-as-list.component.ts
+++ src/app/components/chatbot-as-list/chatbot-as-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Chatbot } from 'src/app/models/chatbot.model';
+import { ChatbotService } from 'src/app/services/chatbot.service';
 
 @Component({
   selector: 'app-chatbot-as-list',
@@ -7,7 +9,15 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ChatbotAsListComponent implements OnInit {
   @Input() chatbot: any;
-  constructor() {}
+  constructor(private chatbotService: ChatbotService) {}
 
   ngOnInit(): void {}
+
+  setFavorite(chatbot: Chatbot): void {
+    this.chatbotService.setFavorite(chatbot);
+  }
+
+  removeFavorite(chatbot: Chatbot): void {
+    this.chatbotService.removeFavorite(chatbot);
+  }
 }
