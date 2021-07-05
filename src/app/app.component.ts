beb413ac73405a46e2de5c1ee6e6426950b7e511
patrickvrb <patrickvrb@gmail.com>
2021-07-04 21:48:20 -0300

    Redirecionamento no ícone do toolbar. Ajuste max-width html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'contact-list';
+
+  constructor(private router: Router) {}
+
+  home(): Promise<boolean> {
+    return this.router.navigate(['']);
+  }
 }
